92c3612bc04bad99b5806fa9b44e8205d26d421b
Paulo Suzart <paulosuzart@gmail.com>
2015-04-03 21:44:45 -0300

    Added command line and better README

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@
 extern crate hyper;
 extern crate rustc_serialize;
 
-
+extern crate getopts;
+use getopts::Options;
+use std::env;
 mod header;
 mod mashape;
 use std::io::Read;
@@ -14,6 +16,10 @@ use hyper::header::ConnectionOption;
 use mashape::CurrencyResponse;
 use std::str::FromStr;
 
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} [options]", program);
+    print!("{}", opts.usage(&brief));
+}
 
 fn fetch () -> f32 {
     let key_str: &'static str = env!("MASHAP_KEY");
@@ -32,11 +38,32 @@ fn fetch () -> f32 {
 
 
 fn main () {
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+    let mut opts = Options::new();
+    opts.optopt("d", "", "Delta between cycles.", "DELTA");
+    opts.optflag("h", "help", "print this help menu");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => { panic!(f.to_string()) }
+    };
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+    let delta = match matches.opt_str("d") {
+        Some(v) => { f32::from_str(v.as_ref()).unwrap() }
+        None => {
+            println!("Will use default value for delta: 10 cents");
+            10.0
+        }
+    };
 
-    let mut args = std::env::args();
+    //let delta = f32::from_str(deltaOpt.unwrap().as_ref()).unwrap();
+    // let mut args = std::env::args();
     let mut last_collected = 0.0;
-    args.next();
-    let delta = f32::from_str(args.next().unwrap().as_ref()).unwrap();
+    // args.next();
+    // let delta = f32::from_str(args.next().unwrap().as_ref()).unwrap();
 
 
     loop {
